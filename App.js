db3fff25172ae64cf591b6765dc27a42e1a1fe04
Nicodemuss <vitornicodemus@gmail.com>
2021-12-09 03:52:16 -0300

    baaaaaaaaaaah

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import Home from './src/pages/Home/Home';
+import Login from './src/pages/Login/Login';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+  const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open coiso teste aaaa ta aterde</Text>
-      <StatusBar style="auto" />
-    </View>
+
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Login" component={Login} />
+          <Tab.Screen name="Home" component={Home} />
+        </Tab.Navigator>
+      </NavigationContainer>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
