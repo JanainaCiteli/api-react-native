25d4c6829158724f7e7f1b603dd96b312eb1eef1
Nicodemuss <vitornicodemus@gmail.com>
2021-12-11 21:57:29 -0300

    componentes novos

---
+++ src/pages/Categoria/Categoria.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View,Button } from 'react-native';
+import { styles } from './Style';
+
+export default function Categoria({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <Text>Categorias</Text>
+            
+        </View>
+    )
+}
---
+++ src/pages/Favoritos/Favoritos.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View,Button } from 'react-native';
+import { styles } from './Style';
+
+export default function Favoritos({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <Text>Favoritos</Text>
+            
+        </View>
+    )
+}
