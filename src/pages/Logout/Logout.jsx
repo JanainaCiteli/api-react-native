3644a53f3b0c5a4d9375ae058db5ce8c63256bd9
PatriciaBraganca <patricia.rbraganca@gmail.com>
2021-12-14 00:34:01 -0300

    Criação do novo Logout

--- src/pages/Logout/Logout.jsx
+++ src/pages/Logout/Logout.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import { Text, View,Button } from 'react-native';
 
 
@@ -11,3 +12,34 @@ export default function Logout ({ navigation }) {
         
     )
 }
+=======
+import { Text, View, Button } from 'react-native';
+import { styles } from './Style';
+
+export default function Logout({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Deseja realmente sair?</Text>
+      <Button style={styles.button} title="SAIR" 
+      onPress={()=> navigation.navigate("login")}
+      />
+      <Button style={styles.button} title="VOLTAR" 
+        onPress={()=> navigation.goBack()} 
+        />
+      
+    </View>
+  );
+ /* export default function Voltar({ navigation }) {
+    return (
+      <View style={styles.container}>
+          
+        
+        <Button style={styles.button} title="VOLTAR" 
+        onPress={()=> navigation.goBack()} 
+        />
+      </View>
+    );
+  }*/
+  
+}
+>>>>>>> Stashed changes
